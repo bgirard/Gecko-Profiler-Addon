ec4cd28f3c498108b6227856d95a6fa4e4c09cc2
Benoit Girard <b56girard@gmail.com>
2012-01-20 22:11:15 -0500

    Self update test

--- build.sh
+++ build.sh
@@ -2,11 +2,11 @@
 if [ ! -d "sdk" ]; then
   echo "Must download the sdk first:"
   echo "git clone https://github.com/mozilla/addon-sdk sdk"
-  echo "cd sdk && git checkout 1.4"
+  echo "cd sdk && git checkout 1.4i && cd .."
   exit
 fi
 
 cd sdk
 . ./bin/activate
 cd ..
-cfx xpi --update-link https://example.com/addon/latest
+cfx xpi --update-link https://github.com/bgirard/Gecko-Profiler-Addon/raw/master/geckoprofiler.xpi
