f20fe0b3d0a4d2e33e2c47c769c40f54c1b79783
Benoit Girard <b56girard@gmail.com>
2012-03-02 13:44:10 -0500

    Release 1.2.0

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#cfx run --binary /Applications/FirefoxNightly.app
-cfx run --binary /Users/markus/code/obj-m-debug/dist/NightlyDebug.app
+cfx run --binary /Applications/FirefoxNightly.app
+#cfx run --binary /Users/markus/code/obj-m-debug/dist/NightlyDebug.app
