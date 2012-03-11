5cdfb9a5c2cf12d2cea6f7f3e6a506d71e451f0b
Benoit Girard <b56girard@gmail.com>
2012-03-10 22:02:01 -0500

    active sdk when testing

--- test.sh
+++ test.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
+if [ ! -d "sdk" ]; then
+  echo "Must download the sdk first:"
+  echo "git clone https://github.com/mozilla/addon-sdk sdk"
+  echo "cd sdk && git checkout 1.4i && cd .."
+  exit
+fi
+
+cd sdk
+. ./bin/activate
+cd ..
+
 cfx run --binary /Applications/FirefoxNightly.app
 #cfx run --binary /Users/markus/code/obj-m-debug/dist/NightlyDebug.app
