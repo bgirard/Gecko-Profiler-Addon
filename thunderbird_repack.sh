b2dc978573186252ccb91962e68e6f2498a633f0
Benoit Girard <b56girard@gmail.com>
2012-03-30 16:26:03 -0400

    Add some support for thunderbird testing

---
+++ thunderbird_repack.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+rm -rf tmp
+mkdir tmp
+cp geckoprofiler.xpi tmp
+cd tmp
+unzip geckoprofiler.xpi
+sed 's/ec8030f7-c20a-464f-9b0e-13a3a9e97384/3550f703-e582-4d05-9a08-453d09bdfdc6/g' install.rdf > install.rdf.new
+mv install.rdf.new install.rdf
+sed 's/>9.0/>1.0/g' install.rdf > install.rdf.new
+mv install.rdf.new install.rdf
+sed 's/>10./>15./g' install.rdf > install.rdf.new
+mv install.rdf.new install.rdf
+zip -r ../geckoprofiler_thunderbird.xpi *
