d3e67ce94d5ec99c743d94c11942c6a5f9b625a5
Benoit Girard <b56girard@gmail.com>
2012-04-15 11:35:42 -0400

    Symbolicate properly if arm-eabi-addr2line is in path

--- data/SymbolicateWorker.js
+++ data/SymbolicateWorker.js
@@ -403,7 +403,7 @@ function readSymbolsLinux(reporter, platform, library, unresolvedList, resolvedS
                 // Crude and buggy approach at getting the file name, fix me
                 var lib = library.name.split("/");
                 lib = lib[lib.length-1];
-                cmd = "/bin/bash -l -c 'arm-eabi-addr2line \"" + lib + "\" " + unresolvedSymbols.join(" ") + "'";
+                cmd = "/bin/bash -l -c 'arm-eabi-addr2line -C -f -e \"/tmp/" + lib + "\" " + unresolvedSymbols.join(" ") + "'";
                 dump(cmd + "\n");
             }
 
@@ -413,7 +413,7 @@ function readSymbolsLinux(reporter, platform, library, unresolvedList, resolvedS
             var outLines = addr2lineResult.split("\n");
             for (var i = 0; i < unresolvedSymbols.length; i++) {
                 if (i*2+1 < outLines.length) {
-                    resolvedSymbols[buckets[j][i]] = outLines[i*2+0] + " " + outLines[i*2+1];
+                    resolvedSymbols[buckets[j][i]] = outLines[i*2+0] + " " + outLines[i*2+1] + " (in " + library.name + ")";
                 } else {
                     resolvedSymbols[buckets[j][i]] = "Unknown (in " + library.name + ")";
                     //resolvedSymbols[buckets[j][i]] = buckets[j][i] + " (" + unresolvedSymbols[i] + "@" + library.name + ")";
