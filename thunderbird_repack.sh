b666ca6dfb6d486fb671ed7b7f0f18016136b9e8
Mike Conley <mconley@mozilla.com>
2012-06-14 10:55:52 -0400

    Getting the profiler to work on Thunderbird - work in progress...

--- thunderbird_repack.sh
+++ thunderbird_repack.sh
@@ -8,6 +8,6 @@ sed 's/ec8030f7-c20a-464f-9b0e-13a3a9e97384/3550f703-e582-4d05-9a08-453d09bdfdc6
 mv install.rdf.new install.rdf
 sed 's/>9.0/>1.0/g' install.rdf > install.rdf.new
 mv install.rdf.new install.rdf
-sed 's/>10./>15./g' install.rdf > install.rdf.new
+sed 's/>10./>16./g' install.rdf > install.rdf.new
 mv install.rdf.new install.rdf
 zip -r ../geckoprofiler_thunderbird.xpi *
