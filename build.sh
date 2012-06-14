b1ef5a8035f81db8be6313cf902da7562a53b761
Mike Conley <mconley@mozilla.com>
2012-06-14 16:53:58 -0400

    Bump Add-on SDK version to 1.7.

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 if [ ! -d "sdk" ]; then
   echo "Must download the sdk first:"
   echo "git clone https://github.com/mozilla/addon-sdk sdk"
-  echo "cd sdk && git checkout 1.4i && cd .."
+  echo "cd sdk && git checkout 1.7 && cd .."
   exit
 fi
 
