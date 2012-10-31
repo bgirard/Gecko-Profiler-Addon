ec4ace61971a63dd9818815d8148274706906dff
Benoit Girard <b56girard@gmail.com>
2012-10-31 17:06:19 -0400

    Add support for a target error message

--- data/sps_panel.js
+++ data/sps_panel.js
@@ -114,6 +114,13 @@ self.port.on("change_status", function(val) {
 });
 document.getElementById("btnToggleActive").onclick = sps_toggle_active;
 
+self.port.on("target_error", function(val) {
+    dump("taget error\n");
+    document.getElementById("divTargetLog").style.display = "";
+    document.getElementById("divTargetControls").style.display = "none";
+    document.getElementById("divTargetLog").innerHTML = "Log:<br>val;
+    dump("taget errored\n");
+});
 
 function bugzilla_file_bug() {
     self.port.emit("filebug", "test");
