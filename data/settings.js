cf9d012a90a9dbb58993e43974cbbaec8c721c02
Benoit Girard <bgirard@mozilla.com>
2012-12-17 17:49:46 -0500

    Update the watchdog feature

--- data/settings.js
+++ data/settings.js
@@ -47,7 +47,8 @@ var featureDescription = {
   "stackwalk" : "Enable stackwalking in <a href='http://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-profiling/'>profiling-nightly</a> or custom profiling builds. Custom builds should specify: 'ac_add_options --enable-profiling'",
   "jank" : "Only record samples when the application is not responding. Useful for collecting the source of hangs over a large timespan.",
   "adb" : "Profile debug version of fennec, this will not work on nightlies. Install 'adb' on your path, and connect a device via usb debugging.",
-  "sampling" : "Adjust frequency of sampling and sample buffer size by choosing a preset or entering custom values:"
+  "sampling" : "Adjust frequency of sampling and sample buffer size by choosing a preset or entering custom values:",
+  "watchdog" : "Opt-in to background performance monitoring. The profiler will watch startup/shutdown and navigation and will look for interesting profiles such as slow shutdown and will send in a detailed execution trace.<br><br><B>PRIVACY WARNGING:</b> Profiles will be sent automactially in the background. This is a major privacy concern and thus is on a volunteer basis only."
 };
 
 var rowCount = 0;
@@ -197,7 +198,7 @@ function selectTabSimple(mainAreaDiv) {
   featuresDiv.textContent = "Features";
   rowCount = 0;
 
-  var feature_stackwalking = addFeatureDiv(mainAreaDiv, "Stackwalk", "stackwalk", "Mouse over text");
+  var feature_stackwalking = addFeatureDiv(mainAreaDiv, "Stackwalk", "stackwalk", "");
   var feature_jank = addFeatureDiv(mainAreaDiv, "Jank", "jank", "Mouse over text");
 }
 function selectTabAdvanced(mainAreaDiv) {
@@ -207,7 +208,8 @@ function selectTabAdvanced(mainAreaDiv) {
   featuresDiv.textContent = "Advanced Features";
   rowCount = 0;
 
-  var feature_stackwalking = addFeatureDiv(mainAreaDiv, "Fennec profiling", "adb", "Mouse over text");
+  var feature_stackwalking = addFeatureDiv(mainAreaDiv, "Fennec profiling", "adb", "");
+  var feature_watchdog = addFeatureDiv(mainAreaDiv, "Profiler Watchdog", "watchdog", "The watchdog is a feature that will profile your browser during start/shutdown and navigation and send in any profiles deemed interesting automatically.");
   var sampling_settings = addSamplingSettingsDiv(mainAreaDiv, "sampling", "Adjust how sampling is done");
 }
 
