b0386b2f9caa353bc04b1d76104b429b4cfd8f72
Benoit Girard <bgirard@mozilla.com>
2013-02-28 16:32:28 -0500

    Update 1.11.12 with newest SDK.

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,7 @@ hash git 2> /dev/null || {
 
 git clone https://github.com/mozilla/addon-sdk sdk
 cd sdk
-git checkout 1.12
+git checkout 1.13.2
 mkdir packages
 cd packages
 git clone https://github.com/erikvold/vold-utils-jplib
