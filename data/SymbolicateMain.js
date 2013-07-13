d5808578645ec5c597a044259dabd2ea88978ba3
Jim Chen <nchen@mozilla.com>
2013-07-12 21:10:23 -0400

    Fix Eideticker symbolication
    The changes mostly deal with symbolication code invoked through xpcshell, so they shouldn't really affect regular profiler operation. symbolicate.sh is deleted and its code is moved to inside eideticker (the shell script was only used by eideticker anyways). Other changes include the ability to set the location of shared libraries through command line arguments.

--- data/SymbolicateMain.js
+++ data/SymbolicateMain.js
@@ -2,6 +2,7 @@ Cu.import("resource://gre/modules/Services.jsm");
 Cu.import("resource://gre/modules/AddonManager.jsm");
 
 dump("Symbolicate: " + arguments[0] + "\n");
-symbolicate_file(arguments[0]);
+// symbolicate_file(profile, libsPrefix, appName)
+symbolicate_file(arguments[0], arguments[1], arguments[2])
 
 //let symbolicateModule = Cu.import("resource://gre/modules/SymbolicateModule.jsm");
