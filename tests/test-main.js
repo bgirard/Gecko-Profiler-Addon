3d6bd8ab0ec064bda75eca29aa6aa7317f3c7a7e
Benoit Girard <b56girard@gmail.com>
2015-01-20 18:37:52 -0500

    Fix async jetpack test. Add a profile dump test

--- tests/test-main.js
+++ tests/test-main.js
@@ -1,21 +1,60 @@
 const m = require("main");
 const self = require("self");
+const tabs = require("sdk/tabs");
+
+function startTest(test) {
+  test.waitUntilDone(5000);
+  m.main();
+}
+
+function stopTest(test) {
+  m.onUnload();
+
+  for (let tab of tabs) {
+    if (tab.url.indexOf("cleopatra") != -1) {
+      tab.close();
+    }
+  }
+
+  test.done();
+}
 
 exports.smokeTest = function(test) {
-  m.main();
+  startTest(test);
+
   m.getProfiler().isActive(function(val) {
     test.assert(val);
-    m.onUnload();
+
+    stopTest(test);
   });
 };
 
-exports.testHarness2 = function(test) {
-  m.main();
+exports.openCleopatra = function(test) {
+  startTest(test);
+
+  m.getAppWrapper().open_cleopatra(null, null, function complete_callback(aProfile) {
+    aProfile = JSON.parse(aProfile);
+
+    // Here we should have a symbolicated profile!
+    test.assert(aProfile != null);
+    test.assert(aProfile['format'] === "profileJSONWithSymbolicationTable,1");
+    test.assert(aProfile.profileJSON != null);
+    test.assert(aProfile.profileJSON != null);
+    test.assert(aProfile.profileJSON.libs != null);
+
+    stopTest(test);
+  });
+};
+
+exports.viewProfile = function(test) {
+  startTest(test);
+
   m.getProfiler().getFeatures(function(features) {
     test.assert(features.length > 0);
     // js is a core profiler features and should always be there
     test.assert(features.indexOf("js") != -1);
-    m.onUnload();
+
+    stopTest(test);
   });
 };
 
