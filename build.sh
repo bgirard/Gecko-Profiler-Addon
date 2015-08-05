19ffcd003268fed66dae6b7f515e51fcab1fc414
Benoit Girard <b56girard@gmail.com>
2015-08-05 16:05:35 -0400

    Stop using old jetpack modules

--- build.sh
+++ build.sh
@@ -2,10 +2,11 @@
 
 set -e
 
+rm -rf geckoprofiler.xpi
 jpm xpi
-sh thunderbird_repack.sh
-mv jid0-edalmuivkozlouyij0lpdx548bc@jetpack-1.16.6.xpi geckoprofiler.xpi
-mv geckoprofiler.xpi geckoprofiler_amo.xpi
+#sh thunderbird_repack.sh
+mv jid0-edalmuivkozlouyij0lpdx548bc@jetpack-1.16.7.xpi geckoprofiler.xpi
+#cp geckoprofiler.xpi geckoprofiler_amo.xpi
 
-jpm xpi --update-link https://github.com/bgirard/Gecko-Profiler-Addon/raw/master/geckoprofiler.xpi --update-url https://github.com/bgirard/Gecko-Profiler-Addon/raw/master/geckoprofiler.update.rdf
-sh thunderbird_repack.sh
+#jpm xpi https://github.com/bgirard/Gecko-Profiler-Addon/raw/master/geckoprofiler.xpi --update-url https://github.com/bgirard/Gecko-Profiler-Addon/raw/master/geckoprofiler.update.rdf
+#sh thunderbird_repack.sh
--- thunderbird_repack.sh
+++ thunderbird_repack.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+set -e 
+
 rm -rf tmp
 mkdir tmp
 cp geckoprofiler.xpi tmp
