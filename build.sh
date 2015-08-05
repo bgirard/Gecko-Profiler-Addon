6988f915bd3232516673b243fe0bc4256a4d7b0d
Benoit Girard <b56girard@gmail.com>
2015-08-05 16:07:13 -0400

    Remove the screen recorder binary

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -e
 rm -rf geckoprofiler.xpi
 jpm xpi
 #sh thunderbird_repack.sh
-mv jid0-edalmuivkozlouyij0lpdx548bc@jetpack-1.16.7.xpi geckoprofiler.xpi
+mv jid0-edalmuivkozlouyij0lpdx548bc@jetpack-1.16.8.xpi geckoprofiler.xpi
 #cp geckoprofiler.xpi geckoprofiler_amo.xpi
 
 #jpm xpi https://github.com/bgirard/Gecko-Profiler-Addon/raw/master/geckoprofiler.xpi --update-url https://github.com/bgirard/Gecko-Profiler-Addon/raw/master/geckoprofiler.update.rdf
