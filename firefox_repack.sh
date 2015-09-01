72818ab5b3116d7ca06147a5d3cb96ac41e3ec4e
Ehsan Akhgari <ehsan@mozilla.com>
2015-09-01 09:24:37 -0400

    Add a Firefox repack script that adds the update url to the install manifest

---
+++ firefox_repack.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Repack the extension to add an update url to the install manifest.
+
+set -e 
+
+cd src
+rm -rf tmp
+mkdir tmp
+cp geckoprofiler.xpi tmp
+cd tmp
+unzip -q geckoprofiler.xpi
+rm geckoprofiler.xpi
+# Find the first instance of </em:targetApplication>, and insert the Thunderbird
+# app description right after.
+sed -e '/<\/em:targetApplication>/r ../fx_install.rdf' install.rdf > install.rdf.new
+mv install.rdf.new install.rdf
+zip -q -r ../geckoprofiler.xpi *
+echo "Firefox repack completed."
