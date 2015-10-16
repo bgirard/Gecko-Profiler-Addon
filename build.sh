c6a12b0718d2b5749e01c0bb75e33778ad93bc68
Benoit Girard <b56girard@gmail.com>
2015-10-16 11:35:53 -0400

    Fix build script

--- build.sh
+++ build.sh
@@ -3,10 +3,10 @@
 set -e
 
 cd src
-rm -rf geckoprofiler.xpi
+rm -rf geckoprofiler.xpi geckoprofiler-unsigned.xpi
 jpm xpi
 #sh thunderbird_repack.sh
-mv jid0-edalmuivkozlouyij0lpdx548bc@jetpack-1.16.9.xpi geckoprofiler.xpi
+mv jid0-edalmuivkozlouyij0lpdx548bc@jetpack-*.xpi geckoprofiler.xpi
 #cp geckoprofiler.xpi geckoprofiler_amo.xpi
 
 #jpm xpi https://github.com/bgirard/Gecko-Profiler-Addon/raw/master/geckoprofiler.xpi --update-url https://github.com/bgirard/Gecko-Profiler-Addon/raw/master/geckoprofiler.update.rdf
@@ -15,4 +15,3 @@ mv jid0-edalmuivkozlouyij0lpdx548bc@jetpack-1.16.9.xpi geckoprofiler.xpi
 cd ..
 ./firefox_repack.sh
 mv src/geckoprofiler.xpi src/geckoprofiler-unsigned.xpi
-git checkout -f src/geckoprofiler.xpi
