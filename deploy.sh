466434927affcde8cf2219ffaf25eb86d77233ca
Benoit Girard <b56girard@gmail.com>
2015-12-04 15:18:53 -0500

    Remove commit script. It's no longer needed

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd src
-ssh webadmin@varium.fantasytalesonline.com "cd tomcat/webapps/ROOT/cleopatra/Gecko-Profiler-Addon&& echo Pull \$PWD && git pull"
-ssh bgirard@people.mozilla.org "cd public_html/Gecko-Profiler-Addon && echo Pull \$PWD && git pull && chmod -R 755 ."
-ssh cleopatra@cleopatra.io "./update-site.sh"
