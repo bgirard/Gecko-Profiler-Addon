952a925e9503228ce78578b7b371248c33dc9408
Benoit Girard <b56girard@gmail.com>
2016-08-24 17:03:59 -0400

    Update profiler to 1.16.18

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ cd src
 rm -rf geckoprofiler.xpi geckoprofiler-unsigned.xpi
 jpm xpi
 #sh thunderbird_repack.sh
-mv jid0-edalmuivkozlouyij0lpdx548bc@jetpack-*.xpi geckoprofiler.xpi
+#mv jid0-edalmuivkozlouyij0lpdx548bc@jetpack-*.xpi geckoprofiler.xpi
 #cp geckoprofiler.xpi geckoprofiler_amo.xpi
 
 #jpm xpi https://github.com/bgirard/Gecko-Profiler-Addon/raw/master/geckoprofiler.xpi --update-url https://github.com/bgirard/Gecko-Profiler-Addon/raw/master/geckoprofiler.update.rdf
